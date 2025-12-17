8e150cfa144c1025a58e002577d4944664f7a276
JIMMY <447841059@qq.com>
2025-12-18 00:10:32 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http', // 注意：你的图片用的是 http，不是 https，必须写对
-        hostname: 'devimgtest.innourl.com', // 核心：图片的域名
-        pathname: '/**', // 匹配该域名下所有路径（** 是通配符）
-      },
-      {
-        protocol: 'https', 
-        hostname: 'file.poetize.cn', 
-        pathname: '/**', 
-      },
-      {
         protocol: 'https', 
         hostname: 'res.cloudinary.com', 
         pathname: '/**', 
--- src/components/custom-antd/Avatar.tsx
+++ src/components/custom-antd/Avatar.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 const App: React.FC<Props> = (props:Props) => {
   const {size="default",shape="circle"} = props;
-  return <Avatar size={size} shape={shape} src="http://devimgtest.innourl.com/SAAS_IMAGE/images/INNOVATION/user/avatar/20251011/20251011174558588_1883131.jpeg" />
+  return <Avatar size={size} shape={shape} src="/jimmy.jpeg" />
 }
 
 export default App;
